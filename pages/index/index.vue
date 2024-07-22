fe26a508403b60e270a50a2a2ab1ba5cc7d4372d
kiDeng <2282960866@qq.com>
2024-07-23 01:48:52 +0800

    Feat:提交首页的编写，增加圆形数据图及数据概览模块

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,6 +1,47 @@
 <template>
 	<view class="content">
-		<view>我是首页</view>
+		<view class="statistics-parent">
+			<text style="font-weight: bold;">数据概览</text>
+			<view class="statistics-item-parent">
+
+				<view class="statistics-item first-item">
+					<text class="statistics-item-title">今日收款（元）</text>
+					<text class="statistics-item-text">{{todyMoney}}</text>
+				</view>
+
+				<view class="statistics-item second-item">
+					<text class="statistics-item-title">今日进出（次）</text>
+					<text class="statistics-item-text">{{todyComeIn}}</text>
+				</view>
+
+				<view class="statistics-item third-item">
+					<text class="statistics-item-title">今日出场（次）</text>
+					<text class="statistics-item-text">{{todyGoOut}}</text>
+				</view>
+
+				<view class="statistics-item fourth-item">
+					<text class="statistics-item-title">本月车流量（次）</text>
+					<text class="statistics-item-text">{{monthCar}}</text>
+				</view>
+
+				<view class="statistics-item fiveth-item">
+					<text class="statistics-item-title">本月营收额（元）</text>
+					<text class="statistics-item-text">{{monthMoney}}</text>
+				</view>
+
+			</view>
+
+		</view>
+
+		<template>
+		  <view class="charts-box">
+		    <qiun-data-charts 
+		      type="pie"
+		      :opts="opts"
+		      :chartData="chartData"
+		    />
+		  </view>
+		</template>
 	</view>
 </template>
 
@@ -8,42 +49,131 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				todyMoney: 35,
+				todyComeIn: 142,
+				todyGoOut: 263,
+				monthCar: 14721,
+				monthMoney: 3265,
+				chartData: {}
 			}
+		},
+		opts: {
+			color: ["#1890FF", "#91CB74"],
+			padding: [5, 5, 5, 5],
+			enableScroll: false,
+			extra: {
+				pie: {
+					activeOpacity: 0.5,
+					activeRadius: 10,
+					offsetAngle: 0,
+					labelWidth: 15,
+					border: true,
+					borderWidth: 3,
+					borderColor: "#FFFFFF"
+				}
+			}
+		},
+		onReady() {
+			this.getServerData();
 		},
 		onLoad() {
 
 		},
 		methods: {
-
+			getServerData() {
+				//模拟从服务器获取数据时的延时
+				setTimeout(() => {
+					//模拟服务器返回数据，如果数据格式和标准格式不同，需自行按下面的格式拼接
+					let res = {
+						series: [{
+							data: [{
+								"name": "微信",
+								"value": 3145,
+								"labelText":"3145元"
+							}, {
+								"name": "支付宝",
+								"value": 120,
+								"labelText":"120元"
+							}]
+						}]
+					};
+					this.chartData = JSON.parse(JSON.stringify(res));
+				}, 500);
+			},
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
 	.content {
 		display: flex;
 		flex-direction: column;
-		align-items: center;
-		justify-content: center;
+		background-color: lightgray;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
+	.statistics-parent {
+		display: inline-flex;
+		flex-direction: column;
+		padding: 10px;
+		margin: 10px;
+		background-color: white;
+		border-radius: 5px;
 	}
 
-	.text-area {
-		display: flex;
-		justify-content: center;
+	.statistics-item-parent {
+		display: inline-flex;
+		flex-direction: row;
+		flex-wrap: wrap;
 	}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	.statistics-item-title {
+		color: white;
+		font-size: 12px;
 	}
+
+	.statistics-item-text {
+		color: white;
+		font-weight: bold;
+		margin-top: 5px;
+	}
+
+	.statistics-item {
+		display: inline-flex;
+		flex-direction: column;
+		padding: 10px;
+		margin-top: 10px;
+		border-radius: 5px;
+	}
+
+	.first-item {
+		width: 90px;
+		background-image: linear-gradient(120deg, #f6d365, #fda085 100%);
+		margin-right: 10px;
+	}
+
+	.second-item {
+		width: 90px;
+		background-image: linear-gradient(135deg, #2AFADF 10%, #4C83FF 100%);
+		margin-right: 10px;
+	}
+
+	.third-item {
+		width: 90px;
+		background-image: linear-gradient(135deg, #ABDCFF 10%, #0396FF 100%);
+	}
+
+	.fourth-item {
+		width: 149px;
+		background-image: linear-gradient(120deg, #f6d365, #fda085 100%);
+		margin-right: 10px;
+	}
+
+	.fiveth-item {
+		width: 149px;
+		background-image: linear-gradient(135deg, #2AFADF 10%, #4C83FF 100%);
+	}
+	.charts-box {
+	    width: 100%;
+	    height: 300px;
+	  }
 </style>
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -3,15 +3,78 @@ const common_vendor = require("../../common/vendor.js");
 const _sfc_main = {
   data() {
     return {
-      title: "Hello"
+      todyMoney: 35,
+      todyComeIn: 142,
+      todyGoOut: 263,
+      monthCar: 14721,
+      monthMoney: 3265,
+      chartData: {}
     };
+  },
+  opts: {
+    color: ["#1890FF", "#91CB74"],
+    padding: [5, 5, 5, 5],
+    enableScroll: false,
+    extra: {
+      pie: {
+        activeOpacity: 0.5,
+        activeRadius: 10,
+        offsetAngle: 0,
+        labelWidth: 15,
+        border: true,
+        borderWidth: 3,
+        borderColor: "#FFFFFF"
+      }
+    }
+  },
+  onReady() {
+    this.getServerData();
   },
   onLoad() {
   },
-  methods: {}
+  methods: {
+    getServerData() {
+      setTimeout(() => {
+        let res = {
+          series: [{
+            data: [{
+              "name": "微信",
+              "value": 3145,
+              "labelText": "3145元"
+            }, {
+              "name": "支付宝",
+              "value": 120,
+              "labelText": "120元"
+            }]
+          }]
+        };
+        this.chartData = JSON.parse(JSON.stringify(res));
+      }, 500);
+    }
+  }
 };
+if (!Array) {
+  const _easycom_qiun_data_charts2 = common_vendor.resolveComponent("qiun-data-charts");
+  const _component_template = common_vendor.resolveComponent("template");
+  (_easycom_qiun_data_charts2 + _component_template)();
+}
+const _easycom_qiun_data_charts = () => "../../uni_modules/qiun-data-charts/components/qiun-data-charts/qiun-data-charts.js";
+if (!Math) {
+  _easycom_qiun_data_charts();
+}
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
-  return {};
+  return {
+    a: common_vendor.t($data.todyMoney),
+    b: common_vendor.t($data.todyComeIn),
+    c: common_vendor.t($data.todyGoOut),
+    d: common_vendor.t($data.monthCar),
+    e: common_vendor.t($data.monthMoney),
+    f: common_vendor.p({
+      type: "pie",
+      opts: _ctx.opts,
+      chartData: $data.chartData
+    })
+  };
 }
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render]]);
 wx.createPage(MiniProgramPage);
