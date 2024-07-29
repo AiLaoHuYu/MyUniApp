188ee71247613900f302b80aa8d81a5e14add290
kideng <ki@txzing.com>
2024-07-29 17:12:34 +0800

    Feat:修改错误代码

--- unpackage/dist/dev/mp-weixin/pages/login/login.js
+++ unpackage/dist/dev/mp-weixin/pages/login/login.js
@@ -6,23 +6,15 @@ const _sfc_main = {
   },
   methods: {
     login() {
-<<<<<<< HEAD
-      common_vendor.index.switchTab({ url: "/pages/index/index" });
-=======
       common_vendor.index.switchTab({
         url: "/pages/index/index"
       });
->>>>>>> b196415b3b29bf9bba8797a402338ba87eeb9e43
     }
   }
 };
 function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
   return {
-<<<<<<< HEAD
-    a: common_vendor.o((...args) => $options.login && $options.login(...args))
-=======
     a: common_vendor.o(($event) => $options.login())
->>>>>>> b196415b3b29bf9bba8797a402338ba87eeb9e43
   };
 }
 const MiniProgramPage = /* @__PURE__ */ common_vendor._export_sfc(_sfc_main, [["render", _sfc_render], ["__scopeId", "data-v-e4e4508d"]]);
